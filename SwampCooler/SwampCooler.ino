1f35f8d7e6da6c13336f8b09b0f6575fd7b854b8
andy-wittig <Awittig@nevada.unr.edu>
2025-12-12 00:04:21 -0800

    Outlining project requirements

--- SwampCooler/SwampCooler.ino
+++ SwampCooler/SwampCooler.ino
@@ -4,25 +4,26 @@
 #include <Wire.h>
 #include <RTClib.h>
 
-//---Time
+//---Time---
 RTC_DS3231 rtc;
-//-------
 
 String GetTime()
 {
   char time[32];
   DateTime curTime = rtc.now();
-  sprintf(time, "%02d:%02d:%02d %02d/%02d/%02d", curTime.hour(), curTime.minute(), curTime.second(), curTime.day(), curTime.month(), curTime.year());
+  sprintf(time, "%02d:%02d:%02d %02d/%02d/%04d",
+              curTime.hour(), curTime.minute(), curTime.second(),
+              curTime.day(), curTime.month(), curTime.year());
   return String(time);
 }
 
+//---States---
 enum MACHINE_STATE {
   DISABLED,
   IDLE,
   RUNNING,
   ERROR
 };
-
 MACHINE_STATE currentState = IDLE;
 
 enum LED_NAME
@@ -33,19 +34,11 @@ enum LED_NAME
   BLUE
 };
 
-//---LCD
+//---LCD---
 const int rs = 22, en = 24, d4 = 29, d5 = 27, d6 = 25, d7 = 23;
-//Pins on LCD Board:
-//rs --> pin 4
-//en --> pin 6
-//d4 --> pin 11
-//d5 --> pin 12
-//d6 --> pin 13
-//d7 --> pin 14
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-//-----
 
-//---Stepper Motor
+//---Stepper Motor---
 #define STEPPER_PIN_1 9
 #define STEPPER_PIN_2 10
 #define STEPPER_PIN_3 11
@@ -53,41 +46,37 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 #define STEPS 64
 Stepper stepper(STEPS, STEPPER_PIN_1, STEPPER_PIN_3, STEPPER_PIN_2, STEPPER_PIN_4);
-//-----------------
 
-//---3-6v Motor
+//---3-6v Motor---
 #define ENABLE 5
 #define DIRA 3
 #define DIRB 4
-//-------------
+const int FAN_SPEED = 100;
 
-//---Potentiometer Setup
+//---Potentiometer---
 int potPin = A2;
 int potVal = 0;
-//---------------------
 
-//---Water Sensor
+//---Water Sensor---
 int waterPin = A0;
 int waterLevel = 0;
-const int waterThreshhold = 212;
-//--------------
+const int waterThreshhold = 10;
 
-//---Temp and Humidity
+//---Temp & Humidity---
 #define DHT11PIN 7
 dht11 DHT11;
 
 const float tempThreshhold = 24; //In degrees celcius
 float tempInCelcius = 0;
 float humidity = 0;
-//-------------------
 
-//---LEDs
+//---LEDs---
 #define RED_LED_PIN 30
 #define YELLOW_LED_PIN 31
 #define GREEN_LED_PIN 32
 #define BLUE_LED_PIN 33
-//-------
 
+//---Buttons---
 #define START_BUTTON_PIN 8
 int buttonState = 0;
 int lastButtonState = 0;
@@ -100,8 +89,8 @@ volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
 volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
 volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
 volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
-//-------
 
+//---Function Definitions
 void SwitchState(MACHINE_STATE newState);
 void ActivateLed(LED_NAME name);
 void HandleStepper();
@@ -116,37 +105,38 @@ void HandleMachineError();
 
 void setup()
 {
-  //---Time
+  //---Time---
   Wire.begin();
   rtc.begin();
   rtc.adjust(DateTime(F(__DATE__),F(__TIME__)));
 
   U0init(9600); //Init serial connection
 
-  //---LCD
+  //---LCD---
   lcd.begin(16, 2); //Columns and rows
 
-  //---Stepper Motor
-  pinMode(STEPPER_PIN_1, OUTPUT);
+  //---Stepper Motor---
+  pinMode(STEPPER_PIN_1, OUTPUT); //TODO: Cannot use pinmode!
   pinMode(STEPPER_PIN_2, OUTPUT);
   pinMode(STEPPER_PIN_3, OUTPUT);
   pinMode(STEPPER_PIN_4, OUTPUT);
 
-  //---3-6v Motor
+  //---3-6v Motor---
   pinMode(ENABLE, OUTPUT);
   pinMode(DIRA, OUTPUT);
   pinMode(DIRB, OUTPUT);
 
-  //---LEDs
+  //---LEDs---
   pinMode(RED_LED_PIN, OUTPUT);
   pinMode(YELLOW_LED_PIN, OUTPUT);
   pinMode(GREEN_LED_PIN, OUTPUT);
   pinMode(BLUE_LED_PIN, OUTPUT);
 
+  //---Buttons---
   pinMode(START_BUTTON_PIN, INPUT);
 }
 
-//---UART Functions
+//---UART Functions---
 void U0init(unsigned long U0baud)
 {
   unsigned long FCPU = 16000000;
@@ -180,7 +170,7 @@ void PrintMessage(const char message[])
    }
    U0putchar('\n');
 }
-//-------------
+//---------------------
 
 void SwitchState(MACHINE_STATE  newState)
 {
@@ -191,27 +181,25 @@ void SwitchState(MACHINE_STATE  newState)
 
 void ActivateLed(LED_NAME name)
 {
+  //TODO: Can't use digital write here:
   digitalWrite(RED_LED_PIN, name == RED ? HIGH : LOW);
   digitalWrite(YELLOW_LED_PIN, name == YELLOW ? HIGH : LOW);
   digitalWrite(GREEN_LED_PIN, name == GREEN ? HIGH : LOW);
   digitalWrite(BLUE_LED_PIN, name == BLUE ? HIGH : LOW);
 }
 
-void HandleMachineDisabled() //COMPLETE: Don't allow vent control when disabled!
+void HandleMachineDisabled()
 { 
   //Turn on LED
   ActivateLed(YELLOW);
-
   //Turn off fan
-  digitalWrite(DIRA, LOW);
-  digitalWrite(DIRB, LOW);
-  analogWrite(ENABLE, LOW);
-
+  StopFan();
+  //LCD Message
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Disabled :(");
 }
-void HandleMachineIdle() //TODO: Record timestamp of when states transition!
+void HandleMachineIdle()
 { 
   //Change state condition
   if (tempInCelcius > tempThreshhold)
@@ -219,7 +207,6 @@ void HandleMachineIdle() //TODO: Record timestamp of when states transition!
     SwitchState(RUNNING);
     return;
   }
-
   if (waterLevel <= waterThreshhold)
   {
     SwitchState(ERROR);
@@ -228,14 +215,10 @@ void HandleMachineIdle() //TODO: Record timestamp of when states transition!
 
   //Display Temp and Humidity
   TempAndHumToLCD();
-
   //Turn on LED
   ActivateLed(GREEN);
-
   //Turn off fan
-  digitalWrite(DIRA, LOW);
-  digitalWrite(DIRB, LOW);
-  analogWrite(ENABLE, LOW);
+  StopFan();
 }
 void HandleMachineRunning() 
 { 
@@ -246,7 +229,6 @@ void HandleMachineRunning()
     SwitchState(IDLE);
     return;
   }
-
   if (waterLevel < waterThreshhold)
   {
     StopFan();
@@ -256,10 +238,8 @@ void HandleMachineRunning()
 
   //Display Temp and Humidity
   TempAndHumToLCD();
-
   //Turn on LED
   ActivateLed(BLUE);
-
   //On entry start fan motor
   StartFan();
 }
@@ -269,8 +249,7 @@ void HandleMachineError()
   lcd.setCursor(0, 0);
   lcd.print("Water level is");
   lcd.setCursor(0, 1);
-  lcd.print("too low!");
-
+  lcd.print("too low! :O");
   //Turn on LED
   ActivateLed(RED);
 }
@@ -278,11 +257,9 @@ void HandleMachineError()
 void TempAndHumToLCD()
 {
   lcd.clear();
-
   lcd.setCursor(0, 0);
   lcd.print("Humidity %");
   lcd.print(humidity, 2);
-
   lcd.setCursor(0, 1);
   lcd.print("Temp (C): ");
   lcd.print(tempInCelcius, 2);
@@ -290,18 +267,18 @@ void TempAndHumToLCD()
 
 void StopFan()
 {
-  digitalWrite(DIRA, LOW);
-  digitalWrite(DIRB, LOW);
-  analogWrite(ENABLE, LOW);
+  digitalWrite(DIRA, LOW); //TODO
+  digitalWrite(DIRB, LOW); //TODO
+  analogWrite(ENABLE, LOW); //<-- Okay to use here.
 
   String msg = String("Fan Stopped: ") + GetTime();
   PrintMessage(msg.c_str());
 }
 void StartFan()
 {
-  digitalWrite(DIRA, HIGH);
-  digitalWrite(DIRB, LOW);
-  analogWrite(ENABLE, 100);
+  digitalWrite(DIRA, HIGH); //TODO
+  digitalWrite(DIRB, LOW); //TODO
+  analogWrite(ENABLE, FAN_SPEED); //<-- Okay to use here.
 
   String msg = String("Fan Started: ") + GetTime();
   PrintMessage(msg.c_str());
@@ -333,42 +310,31 @@ void loop()
 {
   while (U0kbhit() == 0) { };
 
-  //---Sensor Readings
+  //---Sensor Readings---
   if (allowMonitoring)
   {
-    potVal = analogRead(potPin);
-    //Serial.print("Potentiometer: "); 
-    //Serial.println(potVal);
-
-    waterLevel = analogRead(waterPin);
+    potVal = analogRead(potPin); //TODO: canot use analogRead!
+    waterLevel = analogRead(waterPin); //TODO: canot use analogRead!
     //TODO: Either interupt from comparator or via a sample using the ADC (cannot use ADC library)
-    //Serial.print("Water Level: "); 
-    //Serial.println(waterLevel);
 
     int readDHT11 = DHT11.read(DHT11PIN);
     tempInCelcius = (float)DHT11.temperature;
     humidity = (float)DHT11.humidity;
-    /*
-    Serial.print("Humidity (%): ");
-    Serial.println(humidity, 2);
-    Serial.print("Temperature  (C): ");
-    Serial.println(tempInCelcius, 2);
-    */
   }
-  //-----------
 
-  //Check for start button input -- TODO: Should be monitored using and ISR
-  buttonState = digitalRead(START_BUTTON_PIN);
+  //Check for start button input
+  buttonState = digitalRead(START_BUTTON_PIN); //TODO: Should be monitored using an ISR
+  //can use attachInterrupt()
 
   if (buttonState == HIGH && lastButtonState == LOW) 
   {
     StartButtonPressed();
-    delay(50); //Debounce
+    delay(50); //Debounce TODO: Can't use delay, allowed to use the millis()
   }
   lastButtonState = buttonState;
 
   //State Machine
-  switch (currentState) //COMPLETE: Values should not be monitored when disabled.
+  switch (currentState)
   {
     case (DISABLED):
       HandleMachineDisabled();
@@ -397,7 +363,7 @@ void loop()
     HandleStepper();
   }
 
-  delay(60000); //COMPLETE: Updates every 1 minute!
+  delay(60000); //Update every one minute TODO: Can't use delay
 }
 
 unsigned long lastStepTime = 0;
@@ -410,14 +376,12 @@ void HandleStepper()
   unsigned long now = millis();
 
   if (now - lastStepTime >= stepDelay)
-  {
+  { //Runs async to main loop
     lastStepTime = now;
     stepper.setSpeed(200);
 
-    //TODO: Stepper position must be reported to Serial. Provide data and time!
     if (currentStep < 40) 
-    { 
-      
+    {
       stepPos--;
       String msg = "Moving vent left: " + String(stepPos) + GetTime();
       PrintMessage(msg.c_str());
